45cd4c9dec5be1c62f1b9c1f88203624c5911615
Ying-YJH <2826315773@qq.com>
2020-03-17 16:38:56 +0800

    edit Issus.vue page,and will write ajax to send nodeJS. we have a problem:how to set check-box form mobile-select 2020-03-17

--- src/views/Issus.vue
+++ src/views/Issus.vue
@@ -2,7 +2,7 @@
   <div class="issus">
     <!-- 题目 -->
     <div class="item question">
-      <textarea placeholder="请输入试题"></textarea>
+      <textarea placeholder="请输入试题" v-model="question"></textarea>
     </div>
     <!-- 选项 -->
     <div class="item options">
@@ -11,29 +11,32 @@
         <i class="iconfont icon-tianjia"></i>
       </div>
       
-      <div class="opiton-item" v-for="(item, index) in options" :key="index" >
-        <input type="text" v-model="item.value" placeholder="请输入 (选项) " />
-        <i class="iconfont icon-shanchu" @click="deleteThis(index)"></i>
-      </div>
+      <van-checkbox-group v-model="result">
+        <div class="opiton-item" v-for="(item, index) in options" :key="index" >
+          <input type="text" :class="(anwser===item.sort&&checkType==='单选')?'success':''" v-model="item.value" @input="updateOption" placeholder="请输入 (选项) " />
+          <i class="iconfont icon-shanchu" @click="deleteThis(index)" v-show="checkType==='单选'"></i>
+          <van-checkbox class="checkBox" :name="'checked-'+index" checked-color='#7ad151' v-show="checkType==='多选'"></van-checkbox>
+        </div>
+      </van-checkbox-group>
+      
       
     </div>
     <!-- 答案 -->
     <div class="item anwser">
-      <input id="anwser" type="button" placeholder="请输入 (答案) " value="">
+      <input id="anwser" type="button" :value="anwser?anwser:'请选择答案 (单选)'" v-show="checkType=='单选'">
+      <input id="boxAnwser" type="button" :value="boxAnwser?boxAnwser:'请选择答案 (多选)'" v-show="checkType=='多选'">
 
-      <p id="choiceType" ref="choiceType"></p>
+      <p id="choiceType" ref="choiceType">{{checkType}}</p>
       <i class="iconfont icon-xia" @click="cdAnwser"></i>
     </div>
     <!-- 类型(单选、多选、其他) -->
-    <div class="item choiceType">
-      
-    </div>
+    <div class="item choiceType"></div>
     <!-- 专业 -->
     <div class="item profession"></div>
     <!-- 提交 -->
     <div class="item submit">
-      <input type="button" class="btn-reset" value="重置">
-      <input type="button" class="btn-submit" value="提交">
+      <input type="button" class="btn-reset" @click="reset" value="重置">
+      <input type="button" class="btn-submit" @click="submit" value="提交">
     </div>
     
   </div>
@@ -44,14 +47,23 @@ export default {
   name: 'yun-issus',
   data() {
     return {
+      question: '',
       value: '',
-      mobileSelect: null,
-      checkType: '单选||多选',
-      options: [{id: 1, value: '123'},{id: 2, value: 'abc'}]
+      checkType: '单选',
+      options: [{id: 1, value: '', sort: 'A'}],
+      boxOptions: [{data:[{id:'',value:'',sort:''},{id:1,value:'1',sort:'1'}]}],
+      anwser: '',
+      boxAnwser: '',
+      result: []
     }
   },
-  computed:{
-
+  watch:{
+    result: function(newValue, oldValue){
+      this.boxAnwser = ''
+      newValue.forEach((obj,index) =>{
+        this.boxAnwser += this.alphabat(Number.parseInt(obj.split('-')[1])) + ' '
+      })
+    }
   },
   mounted(){
     let that = this
@@ -61,39 +73,93 @@ export default {
       wheels: [
         {
           data:[
-            {id: '12', value: '张三'},
-            {id: '13', value: '李四李四李四李四李四李四李四李四'}
+            {id: 'c1', value: '单选'},
+            {id: 'c2', value: '多选'}
           ]
         }
       ],
+      keyMap:{
+        id: 'id',
+        value: 'value'
+      },
       position: [0,0],
       callback: function(indexArr, data){
-        console.log(data)
+        that.checkType = data[0].value
+        if(data[0].value === '多选'){
+          that.$toast({
+            message: '请选择答案'
+          })
+        }
+        // that.setBoxAnwser() 
+        // console.log(that.boxOptions[0])
+        // that.selectMoreAnwser.updateWheel(0, [{id:'',value:'',sort:''},{id:2222,value:'aaaaa',sort:'C'}])
+        // that.selectMoreAnwser.updateWheels([{data:[{id:0,value:'',sort:''},{id:2,value:'222',sort:'B'}]}])
+        // that.boxOptions = [
+        //   {data:[{id:'',value:'',sort:''},{id:1,value:'1',sort:'1'}]},
+        //   {data:[{id:'',value:'',sort:''},{id:2,value:'2',sort:'1'}]},
+        //   {data:[{id:'',value:'',sort:''},{id:3,value:'3',sort:'1'}]},
+        //   {data:[{id:'',value:'',sort:''},{id:4,value:'4',sort:'1'}]}
+        // ]
+        // that.selectMoreAnwser.updateWheel(2, [{id:'',value:'',sort:''},{id:2222,value:'aaaaa',sort:'C'}])
       }
     }),
 
     this.selectAnwser = new MobileSelect({
       trigger: '#anwser',
-      title: '选择答案',
+      title: '选择答案(单选)',
       wheels: [
         {
           data: that.options
         }
       ],
       position: [0,0],
+      keyMap:{
+        id: 'id',
+        value: 'value'
+      },
       callback: function(indexArr, data){
-        console.log(that.options)
+        console.log(data)
+        that.anwser = data[0].sort
       }
     })
+
+    this.selectMoreAnwser = new MobileSelect({
+        trigger: '#boxAnwser',
+        title: '选择答案(多选)',
+        triggerDisplayData: false,
+        wheels: that.boxOptions,
+        // wheels:[
+        //   {data:[{id:0,value:'',sort:''},{id:1,value:'111',sort:'A'}]},
+        //   {data:[{id:0,value:'',sort:''},{id:2,value:'222',sort:'B'}]}
+        // ],
+        callback: function(indexArr, data){
+          console.log(data)
+          data.forEach(element => {
+            that.boxAnwser += `${element.sort} `
+          });
+        }
+      })
   },
   methods:{
+    // 加载多选 error
+    // {data:[{id:11,value:'123',sort:'A'}]}}
+    setBoxAnwser(){
+      this.boxOptions = []
+      let that = this
+      this.options.forEach( obj =>{
+        that.boxOptions.push({data:[{id:0,value:'',sort:''},{id:obj.id, value:`${obj.value}`, sort:`${obj.sort}`}]})
+      })
+      // this.selectMoreAnwser.updateWheels(this.boxOptions)
+      
+    },
     // 点击下拉图标转到点击按钮
     cdAnwser(){
       this.$refs.choiceType.click()
     },
     // 添加选项
     addOptions(){
-      this.options.push({id: (this.options.length+1), value: ''})
+      this.options.push({id: (this.options.length+1), value: '', sort: this.alphabat(this.options.length)})
+      this.updateOption()
     },
     // 删除此选项
     deleteThis(index){
@@ -101,18 +167,73 @@ export default {
       this.$dialog.confirm({
         title: '提示',
         message: '是否删除此选项？'
-      }).then(() => {console.log(this.alphabat(3))
-        that.options.splice(index, 1)
+      }).then(() => {
+        this.options.splice(index, 1)
+        // 更新选列号 ABCD...
+        this.options.forEach((obj,index) =>{
+          obj.sort = this.alphabat(index)
+        })
+        this.updateOption()
       }).catch(() => {
-        // this.$toast({
-        //   message: '删除出错！'
-        // })
-        console.log('出错了！')
+        this.$toast({
+          message: '出错了！'
+        })
       })
     },
     // 生成序号 ABCD...
     alphabat(index){
       return String.fromCharCode(65+index);
+    },
+    // 更新选项(mobile-select)
+    updateOption(){
+      this.selectAnwser.updateWheel(0, this.options)
+    },
+    // 提交
+    submit(){
+      // 验证数据
+      if(!this.question.trim()){
+        this.$toast({message:'题目不能为空！'})
+        return ;
+      }else if(this.options.length<2){
+        this.$toast({message:'答案至少需要两项'})
+        return ;
+      }else{
+        let hasAnwser = true
+        this.options.forEach(obj =>{
+          if(!obj.value.trim()){
+            hasAnwser = false
+          }
+        })
+        if(!hasAnwser){
+          this.$toast({message:'选项内容不能为空'})
+          return ;
+        }
+
+        if(this.checkType === '单选'){
+          if(!this.anwser.trim()){
+            this.$toast({message:'请选择答案'})
+            return ;
+          }
+          
+        }else{
+          if(this.boxAnwser.trim().split(' ').length < 2){
+            this.$toast({message:'多选题至少需要两个正确答案'})
+            return ;
+          }
+        }
+      }
+
+      console.log('next to ajax send data...')
+    },
+    // 重置
+    reset(){
+      this.question = ''
+      // this.checkType = '单选'
+      // this.selectType.locatePosition(0,0)
+      this.options = [{id:1, value:'',sort:'A'}]
+      this.anwser = ''
+      this.boxAnwser = ''
+      this.result = []
     }
   }
 }
@@ -207,6 +328,16 @@ export default {
         width: 2rem;
         padding: 1rem;
       }
+      .success{
+        box-shadow: 0 0 10px #a7f781;
+        background: #cbf7b7;
+        // background: #7ad151;
+        color: #666666;
+      }
+      .checkBox{
+        width: 2rem;
+        padding: 1rem;
+      }
     }
   }
   .anwser{
@@ -223,6 +354,7 @@ export default {
     }
     input{
       width: 60%;
+      text-align: left;
     }
     #choiceType{
       // width: 40%;
